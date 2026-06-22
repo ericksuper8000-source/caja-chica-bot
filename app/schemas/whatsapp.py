a927a71daf1b9866d58b55c7496036aca0efdd05
Erick_Dev <ericksuper80@gmail.com>
2026-06-22 16:37:10 -0600

    Formateando archivos para pasar pruebas unitarias

--- app/schemas/whatsapp.py
+++ app/schemas/whatsapp.py
@@ -1,10 +1,12 @@
 from pydantic import BaseModel
 
+
 # Este es el molde básico para entender quién nos escribe y qué nos dice
 class MensajeTexto(BaseModel):
     id: str
     texto: str
     remitente: str  # El número de teléfono de la persona (ej: "50688888888")
+
 
 # Este es el molde del paquete grande que nos enviará Meta
 class WebhookPayload(BaseModel):
