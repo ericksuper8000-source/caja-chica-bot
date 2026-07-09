6b694aa6b9ca2ac7966e699428e579b818b944e7
Erick_Dev <ericksuper80@gmail.com>
2026-07-09 13:25:55 -0600

    Reparacion de secretos privados

--- tests/test_health.py
+++ tests/test_health.py
@@ -1,6 +1,10 @@
 import os
 
+
 def test_environment_variables():
     # Verifica que nuestro conftest.py esté inyectando las variables correctamente
-    assert os.environ.get("DATABASE_URL") == "postgresql://postgres:password@localhost:5432/caja_chica_db"
+    assert (
+        os.environ.get("DATABASE_URL")
+        == "postgresql://postgres:password@localhost:5432/caja_chica_db"
+    )
     assert os.environ.get("ENVIRONMENT") == "test"
