374a7fc4f2c80cd54c7740967b8dc3fb4ffb5908
Erick_Dev <ericksuper80@gmail.com>
2026-07-29 11:23:09 -0600

    testing 1

--- services/sheets_service.py
+++ services/sheets_service.py
@@ -16,7 +16,7 @@ def get_sheets_client() -> Any:
     Inicializa y devuelve el cliente autenticado de gspread.
     """
     try:
-        client = gspread.service_account(filename=settings.GOOGLE_APPLICATION_CREDENTIALS)
+        client = gspread.service_account(filename=settings.GOOGLE_APPLICATION_CREDENTIALS)  # type: ignore[attr-defined]
         return client
     except Exception as e:
         logger.error(f"Error crítico al autenticar con gspread: {e}")
